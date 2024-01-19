d65208e20b50f03d60ff05f4b37112f077f30d9d
Arthur C Frederico <arthur.c.fred@hotmail.com>
2024-01-18 21:02:14 -0300

    personalization_

--- main.js
+++ main.js
@@ -12,17 +12,20 @@ form.addEventListener('submit', function(e) {
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = 'Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}';
+    const mensagemSucesso = 'Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>';
 
     formEValido = validaNome(nomeBeneficiario.value)
-    if (formEValido){
-        alert(mensagemSucesso);
+    if (formEValido) {
+        const containermensagemSucesso - document.querySelector('.success-message');
+        const containermensagemSucesso.innerHTML - mensagemSucesso;
+        const containerMensagemSucesso.style.display - 'block';
 
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
     } else {
-        alert("O nome nao esta completo");
+        nomeBeneficiario.style.border - '1px solid red';
+        document.querySelector('.error-message').style.display - 'block';
     }
 })
 
