58bd6e75cd4e10cf19d732b104380df5111f4421
Arthur C Frederico <arthur.c.fred@hotmail.com>
2024-01-18 21:27:30 -0300

    adjustment in CSS and JS_

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEValido = false;
 
-function validaNome(nomeCompleto){
+function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
     return nomeComoArray.length >= 2;
 }
 
 form.addEventListener('submit', function(e) {
-    let formEValido = false;
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
     const mensagemSucesso = 'Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>';
 
     formEValido = validaNome(nomeBeneficiario.value)
     if (formEValido) {
-        const containermensagemSucesso - document.querySelector('.success-message');
-        const containermensagemSucesso.innerHTML - mensagemSucesso;
-        const containerMensagemSucesso.style.display - 'block';
+        const containermensagemSucesso = document.querySelector('.success-message');
+        containermensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
 
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
     } else {
-        nomeBeneficiario.style.border - '1px solid red';
-        document.querySelector('.error-message').style.display - 'block';
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
     }
 })
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formEValido = validaNome(e.target.value);
 
-
-    /* Quando adicionar let formEValido = false muda a estrutura
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
-    if (!validaNome(nomeBeneficiario.value)) { // ou assim === false ), ao inves do ! como negacao
-        alert("O nome nao esta completo");
+    if (!formEValido) {
+        nomeBeneficiario.classList.add('error');
+        //nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
     } else {
-        alert("Tudo certo")
-    }*/
+        nomeBeneficiario.classList.remove('error');
+        //nomeBeneficiario.style = '';
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
